c5ec8c6aef067c4fb728d5c361e148e0cc54ede2
Inácio Angelino Chimuco Capamba <inacioangelino272@gmail.com>
2026-07-19 08:22:10 -0300

    update

--- BACK-END/src/main/java/com/example/duotec/controller/ProgressoUsuarioController.java
+++ BACK-END/src/main/java/com/example/duotec/controller/ProgressoUsuarioController.java
@@ -6,7 +6,6 @@ import com.example.duotec.service.ProgressoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
